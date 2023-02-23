d9e81bb4d45876173d3f296c60302fa0eea4f211
Josh Lopez-Binder <joshlobinder@gmail.com>
2023-02-23 11:40:29 -0700

    readme, questions and Hs, begins script, .gitignore unzipped

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zuni Snow Pack Accumulation Project
+
+## Are the thinning treatments affecting the snowpack accumulation potential in the Zuñi mountains?
+
+## Instructions
+1. Unzip all .zip files within data
---
+++ R_scripts/01_data_exploration.R
@@ -0,0 +1,25 @@
+##### read me ####
+# the purpose of this script is to visualize spatial tree cover data for the 
+# zuni mountains as well as
+# - describe dataset size
+# - describing data types
+# - checking data distributions
+# - checking for spatial autocorrelation
+# - checking for temporal autocorrelation
+# - checking for correlation between variables <?
+
+#### Libraries ####
+library(tidyverse)
+library(raster)
+#NOTE rgdal is going to be retired by end of 2023. work for now so leaving it
+# See https://r-spatial.org/r/2022/04/12/evolution.html and 
+# https://github.com/r-spatial/evolution
+library(rgdal)
+
+#### load data ####
+p034r035_TC_2015 <- raster("./data/GFCC30TC_2015/GFCC30TC_p034r035_TC_2015/p034r035_TC_2015.tif")
+p034r035_TC_2015
+hasValues(p034r035_TC_2015)
+inMemory(p034r035_TC_2015)
+
+plot(p034r035_TC_2015)
