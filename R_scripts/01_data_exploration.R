adb48cee24930a350f20d613bd27b08177e07946
Josh Lopez-Binder <joshlobinder@gmail.com>
2023-03-01 17:30:37 -0700

    cleans up script a bit

--- R_scripts/01_data_exploration.R
+++ R_scripts/01_data_exploration.R
@@ -1,6 +1,8 @@
 ##### read me ####
 # the purpose of this script is to visualize spatial tree cover data for the 
 # zuni mountains as well as
+# - merge TC images
+# - crop merged TC image to project area
 # - describe dataset size
 # - describing data types
 # - checking data distributions
@@ -28,31 +30,29 @@ p035r035_TC_2015 <- rast("./data/GFCC30TC_2015/GFCC30TC_p035r035_TC_2015/p035r03
 #also appears to be way far away
 p035r036_TC_2015 <- rast("./data/GFCC30TC_2015/GFCC30TC_p035r036_TC_2015/p035r036_TC_2015.tif")
 
+puerco_area_spat <- terra::vect("./data/Puerco Project Area/puerco_Project-polygon.shp")
+
 
 zm <- merge(p035r035_TC_2015, p035r036_TC_2015)
 plot(zm)
 
+#### Crop by Puerco Project Area ####
 
-
-# Load Puerco Project area shp file
-#puerco_area <- st_read(dsn="./data/Puerco Project Area/puerco_Project-polygon.shp")
-puerco_area_spat <- terra::vect("./data/Puerco Project Area/puerco_Project-polygon.shp")
 terra::ext(puerco_area_spat)
-#it appears that projecting sets the extent to the width of the UTM??
 puerco_area_spat <- terra::project(puerco_area_spat, zm)
 terra::ext(puerco_area_spat)
 
-#TODO: figure out how to rasterize so extent is bbox of polygon.
-#right now it is getting extent from zm. Maybe can generate a bbox raster with 30m cell size?
-#dunno.
 puerco_area_raster <- terra::rasterize(puerco_area_spat, zm)
 #manual extent is clunky but it allows trim operation to be wayyy faster
 manual_extent = ext(700000, 750000, 3900000, 3950000) 
 puerco_area_raster <- terra::crop(puerco_area_raster, manual_extent)
-plot(puerco_area_raster)
-puerco_area_raster <- terra::trim(puerco_area_raster)
+#visual check taht we are not cropping parts of the project area
+plot(puerco_area_raster) 
+#remove border of NaN values
+puerco_area_raster <- terra::trim(puerco_area_raster) 
 plot(puerco_area_raster)
 
+#zuni mountains cropped
 zm_c <- terra::crop(zm, puerco_area_raster)
 plot(zm_c)
 plot(puerco_area_raster, add=TRUE)
