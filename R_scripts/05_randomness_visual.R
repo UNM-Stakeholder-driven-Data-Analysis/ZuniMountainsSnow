fc215de3547dc937237d79a74e6b945d27b59e69
Josh Lopez-Binder <joshlobinder@gmail.com>
2023-05-12 15:51:59 -0600

    clears out deprecated code in randomness_visual
    adds scalebar to example sim image

--- R_scripts/05_randomness_visual.R
+++ R_scripts/05_randomness_visual.R
@@ -4,12 +4,13 @@ library(tidyterra)
 library(ggplot2)
 library(cowplot)
 library(gridExtra)
+library(ggpubr)
 source("./R_Scripts/00_Functions.R")
 
 ##### visual example of a stochastic simulation for one 30x30 ####
 coverVal = 30
-mat <- as.vector(MonteCarloMat(coverVal))
-img <- rast(matrix(mat$`sample(vec, size = 100)`, nrow=10, ncol=10))
+mat <- as.vector(MonteCarloMat(coverVal, numSub=10))
+img <- rast(matrix(mat$`sample(vec, size = numCell)`, nrow=10, ncol=10))
 img_fac <- as.factor(img)
 levels(img_fac) <-  data.frame(ID=c(0, 100), cover=c("ground", "tree cover"))
 
@@ -30,89 +31,53 @@ dev.off()
 
 
 #### load data ####
-zm2000_sim <- rast("./R_output/poly2014-09-29/zm2000_sim.tif")
-zm2005_sim <- rast("./R_output/poly2014-09-29/zm2005_sim.tif")
-zm2010_sim <- rast("./R_output/poly2014-09-29/zm2010_sim.tif")
-zm2015_sim <- rast("./R_output/poly2014-09-29/zm2015_sim.tif")
+zm2000_sim <- rast("./R_output/poly2014-09-29/sim3x3/zm2000_sim.tif")
+zm2005_sim <- rast("./R_output/poly2014-09-29/sim3x3/zm2005_sim.tif")
+zm2010_sim <- rast("./R_output/poly2014-09-29/sim3x3/zm2010_sim.tif")
+zm2015_sim <- rast("./R_output/poly2014-09-29/sim3x3/zm2015_sim.tif")
 
 
-
-zm2000_opt_raw <- rast("./R_output/poly2014-09-29/zm2000_opt.tif")
-zm2005_opt <- rast("./R_output/poly2014-09-29/zm2005_opt.tif")
-zm2010_opt <- rast("./R_output/poly2014-09-29/zm2010_opt.tif")
-zm2015_opt <- rast("./R_output/poly2014-09-29/zm2015_opt.tif")
-
-#### distinguish between suboptimal types ####
-
-
-zm2000_opt <- SubOptDistinguish(zm2000_sim, zm2000_opt_raw)
-
-#### convert to factors ####
-#dang probably shold have converted to factor rasters in the first place. oh well
-simLevels <- lapply(1:100, function(x) data.frame(ID=c(0, 100), cover=c("ground", "tree")))
-optLevels <- lapply(1:100, function(x) data.frame(ID=c(-1, 0, 1), cover=c("tree", "bare", "optimal")))
-
-
-zm2000_sim_fac <- as.factor(zm2000_sim)
-levels(zm2000_sim_fac) <- simLevels
-names(zm2000_sim_fac) <- 1:100
-
-zm2000_opt_fac <- as.factor(zm2000_opt)
-levels(zm2000_opt_fac) <- optLevels
-names(zm2000_opt_fac) <- 1:100
 
 #### zoomed in plot sim, opt ####
-
-
 
 cropExt <- ZoomExt(widthMult=4, xCellStart=42, yCellStart=59, img=zm2000_sim)
 cropRect <- as.polygons(cropExt, crs=crs(zm2000_sim))
 
 #REFERENCE OF ZOOM LOCATION
-jpeg("./GeneratedPlots/OneStochasticSim_withZoom.jpeg", height = im.width * aspect.r, width = im.width)
-ggplot() +
-  geom_spatraster(data=zm2000_sim_fac[[1]], maxcell=10e+05) +
-  scale_fill_manual(name = "value", values = c(GROUND, TREE), na.translate=F)+  
-  geom_spatvector(data=cropRect, fill=NA, colour="black", linewidth=0.5) +
-  labs(title="One Stochastic Simulation for the year 2000")
-dev.off()
+simLyr <- 2
 
-ggplot() +
-  geom_spatraster(data=zm2000_opt_fac[[1]], maxcell=10e+05) +
-  scale_fill_manual(name = "value", values = c(SUBTREE, SUBGROUND, OPTIMAL), na.translate=F)+  
-  geom_spatvector(data=cropRect, fill=NA, colour="black")
+p <- PlotSimImage(zm2000_sim[[simLyr]] 
+                  ,paste("One Stochastic Simulation for the year 2000", "\n simulation #", toString(simLyr), sep="")
+                  ,cropRect)
+p <- p +
+  scalebar(x.min=xmin(optAllCriteria)
+           ,x.max=xmax(optAllCriteria)
+           ,y.min=ymin(optAllCriteria)
+           ,y.max=ymax(optAllCriteria)
+           ,dist = 0.5
+           ,dist_unit = "km"
+           ,transform = FALSE 
+           ,model = "WGS84"
+           ,border.size=0.5
+           ,st.size=3
+           ,box.fill="white"
+           ,box.color="grey"
+           ,st.color="grey"
+  ) +
+  labs(y="", x="")
+ggsave("./GeneratedPlots/OneStochasticSim_withZoom.jpeg"
+       ,height=HEIGHT
+       , width=WIDTH
+       ,units="in")
 
-zm2000_sim_crop <- crop(zm2000_sim_fac, cropExt)
-zm2000_opt_crop <- crop(zm2000_opt_fac, cropExt)
+#ggplot() +
+#  geom_spatraster(data=zm2000_opt_fac[[1]], maxcell=10e+05) +
+#  scale_fill_manual(name = "value", values = c(SUBTREE, SUBGROUND, OPTIMAL), na.translate=F)+  
+#  geom_spatvector(data=cropRect, fill=NA, colour="black")
 
-# ZOOM IN COMPARISON OF SIM AND OPT
-p1 <- ggplot() +
-  geom_spatraster(data=zm2000_sim_crop[[1:3]]) + 
-  facet_wrap(~lyr) +
-  labs(title="Simulated Tree Distributions") +
-  scale_fill_manual(name = "value", values = c(GROUND, TREE))+  
-  scale_x_continuous(breaks=1) + #hack to turn off labels + 
-  scale_y_continuous(breaks=1) #hack to turn off labels 
-p2 <- ggplot() +
-   geom_spatraster(data=zm2000_opt_crop[[1:3]]) + facet_wrap(~lyr) +
-  labs(title="Optimal Cells")+ 
-  scale_fill_manual(name = "value", values = c(SUBTREE, SUBGROUND, OPTIMAL))+  
-  scale_x_continuous(breaks=1) + #hack to turn off labels + 
-  scale_y_continuous(breaks=1) #hack to turn off labels
+#zm2000_sim_crop <- crop(zm2000_sim_fac, cropExt)
+#zm2000_opt_crop <- crop(zm2000_opt_fac, cropExt)
 
-jpeg("./GeneratedPlots/zoom_sim_opt_compare.jpeg", height = im.width * aspect.r, width = im.width)
-grid.arrange(p1, p2, ncol = 1, top="year 2000")
-dev.off()
-
-TotArea <- function(imgStack){
-  #in meters squared
-  #note using subst 0 -> NA and then expanse would also work. but this is probably faster
-  numCells <- global(imgStack, fun="sum", na.rm=TRUE)
-  totArea <- numCells * 9 #cells are 3x3m
-  return(totArea)
-}
-
-areaTot<- TotArea(zm2000_opt_crop)
 
 #### alpha sum plot ####
 
@@ -128,15 +93,20 @@ zm2000_sim_alphaSum_crop <- crop(zm2000_sim_alphaSum, cropExt)
 
 p1 <- ggplot() +
   geom_spatraster(data=zm2000_sim_alphaSum, maxcell=10e+05) +
-  geom_spatvector(data=cropRect, fill=NA, colour="white")
+  geom_spatvector(data=cropRect, fill=NA, colour="white", linewidth=0.4)+
+  scale_fill_viridis_c(na.value=NA
+                       ,name="value")
   
   
 p2<- ggplot() + 
   geom_spatraster(data=zm2000_sim_alphaSum_crop) +
+  scale_fill_viridis_c(na.value=NA
+                       ,name="value") +
   labs(title="Zoomed Area")
 
+
 jpeg("./GeneratedPlots/simsSummedAlpha.jpeg", height = im.width * aspect.r, width = im.width)
-grid.arrange(p1, p2, nrow = 1, top="Sum of Tree distributation simulations, where tree values = 1/100, nSimulations=100. Year 2000 ")
+grid.arrange(p1, p2, nrow = 1, top="Sum of Tree distributation simulations, \nwhere tree values = 1/100, nSimulations=100. Year 2000 ")
 dev.off()
 
 
